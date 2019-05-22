be08cf4b996cf497d74ac19ff0ea932606edc3b0
unknown <SUITE@SUITESM708-ST28.cibertec.edu.pe>
2019-05-22 17:41:16 -0500

    add controllers

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 
+import createProject from '../controllers/project.controller';
+
 const router = Router();
 
-
+// /api/projects/
+router.post('/', createProject);
 
 export default router;
